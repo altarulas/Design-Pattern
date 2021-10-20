51311124b9e24a3a0e62c7a4ab848a381bbee6c6
İsmail Altar Ulaş <92596687+altarulas@users.noreply.github.com>
2021-10-20 11:23:55 +0300

    Add files via upload

---
+++ StrategyPattern/src/AmericaShopping.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class AmericaShopping implements Shopping{
+
+    Product price;
+
+    @Override
+    public double calculatePrice(List<Product> products)
+    {
+        int count = 0;
+        int sum = 0;
+
+        while(count < 3)
+        {
+            price = products.get(count);
+            double p = price.getPrice();
+            count++;
+
+            if(products.equals(Size.XL))
+                p = p + (p / 100) + 15 + ((p / 10000) * 75) + 10;
+            else
+                p =  p + (p / 100) + 15 + ((p / 10000) * 75) + 5;
+
+            sum += p;
+
+        }
+
+        return sum;
+    }
+
+    @Override
+    public Currency getCurrency() {
+        return Currency.USD;
+    }
+}
---
+++ StrategyPattern/src/Shopping.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface Shopping {
+
+    double calculatePrice(List<Product> products);
+    Currency getCurrency();
+}
