39e543d25c44e31c421e2099414e571087c07441
İsmail Altar Ulaş <92596687+altarulas@users.noreply.github.com>
2022-01-16 12:25:48 +0300

    Add files via upload

---
+++ StrategyPattern/Cart.java
@@ -0,0 +1,28 @@
+package SecondOne;
+
+import SecondOne.Model.Product;
+import SecondOne.Model.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+    private Shopping shopping;
+    List<Product> products = new ArrayList<>();
+
+    public Cart(Shopping shopping)
+    {
+        this.shopping = shopping;
+    }
+
+    public void addProduct(Product product)
+    {
+        this.products.add(product);
+    }
+
+    public double getTotalPrice()
+    {
+        return shopping.calculatePrice(products);
+    }
+}
