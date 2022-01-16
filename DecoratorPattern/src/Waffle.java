424ae3292fe873740e346c78465a845521a99ebe
İsmail Altar Ulaş <92596687+altarulas@users.noreply.github.com>
2022-01-16 12:19:46 +0300

    Delete DecoratorPattern directory

--- DecoratorPattern/src/Waffle.java
+++
@@ -1,6 +0,0 @@
-public interface Waffle {
-
-    public String getDescription();
-    public double getPrice();
-
-}
--- DecoratorPattern/src/WaffleDecorator.java
+++
@@ -1,6 +0,0 @@
-public abstract class WaffleDecorator implements Waffle{
-
-    @Override
-    public abstract String getDescription();
-
-}
