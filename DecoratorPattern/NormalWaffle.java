2c8e2c5c52906b3262ad76fc42732b0b6456bf74
İsmail Altar Ulaş <92596687+altarulas@users.noreply.github.com>
2022-01-16 12:28:41 +0300

    Add files via upload

---
+++ DecoratorPattern/NormalWaffle.java
@@ -0,0 +1,12 @@
+public class NormalWaffle implements Waffle{
+
+    @Override
+    public String getDescription() {
+        return "Normal Waffle, ";
+    }
+
+    @Override
+    public double getPrice() {
+        return 13.0;
+    }
+}
